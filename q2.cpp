c85a95aecc37394aa5fc5ad9589c410b038f6ff2
JustinPalm1 <jlp0102@auburn.edu>
2024-10-28 13:38:22 -0500

    Solution q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -8,14 +8,40 @@ class Temperature {
 		       degreesFahrenheit,
 		       degreesCelsius;
 	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
+		void setTempKelvin(double degreesKelvin) { 
+            		degreesCelsius = degreesKelvin - 273.15; // Convert to Celsius
+            		degreesFahrenheit = (degreesCelsius * 9.0 / 5.0) + 32; // Convert to Fahrenheit
+		}
+		double getTempKelvin() { 
+			return degreesKelvin;
+		}
 		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
+		double getTempCelsius() { 
+			return degreesCelsius;
+		} 
 		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		double getTempFahrenheit() { 
+			return degreesFahrenheit;
+		}
 };
 
 int main() {
+    // Define variable
+    double degreesKelvin;
 
+    // Create an object of Temperature
+    Temperature temp;
+
+    // Prompt user for input
+    cout << "Enter a temperature in degrees Kelvin: ";
+    cin >> degreesKelvin;
+
+    // Set the temperature in Kelvin
+    temp.setTempKelvin(degreesKelvin);
+
+    // Display the temperatures in Celsius and Fahrenheit
+    cout << "Temperature in Celsius: " << temp.getTempCelsius() << "°C" << endl;
+    cout << "Temperature in Fahrenheit: " << temp.getTempFahrenheit() << "°F" << endl;
+
+    return 0;
 }
